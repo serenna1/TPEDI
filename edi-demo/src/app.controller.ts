2675cbf4ccefcc5296ac1b9bc7a5a7cae55e7aff
serenna1 <rotondaserena3@gmail.com>
2025-09-01 20:00:59 -0300

    nest

---
+++ edi-demo/src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('api')
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Get()
+  getByeBye(): string {
+    return this.appService.getHello();
+  }
+}
